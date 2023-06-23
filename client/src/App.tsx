77bedd07f2725d0b50c4daf0b56ed0cd640fd68e
Mustafa Salem <mustafaissasalem@gmail.com>
2023-06-23 10:37:22 +0300

    feat: created the aws s3 upload functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,44 @@
-import { FC } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
+import axios from 'axios';
 
-const App: FC = () => <h1>Mustafa Salem</h1>;
+const App: FC = () => {
+  const [files, setFiles] = useState<FileList | null>(null);
+  const [message, setMessage] = useState<string>('');
+
+  const onSubmit = async (): Promise<any> => {
+    const file = files?.[0] as File;
+
+    const res = await axios.get(`http://localhost:8080/api/v1/upload?fileName=${file.name}`);
+
+    await axios.put(res.data.presignedUrl, file);
+  };
+
+  return (
+    <div>
+      <input
+        name="message"
+        type="text"
+        value={message}
+        onChange={
+          (e: ChangeEvent<HTMLInputElement>) => setMessage(e.target.value)
+        }
+      />
+      <br />
+      <input
+        name="files"
+        type="file"
+        onChange={
+          (e: ChangeEvent<HTMLInputElement>) => setFiles(e.target.files)
+        }
+      />
+      <br />
+      <input
+        type="submit"
+        value="Submit"
+        onClick={onSubmit}
+      />
+    </div>
+  );
+};
 
 export default App;
